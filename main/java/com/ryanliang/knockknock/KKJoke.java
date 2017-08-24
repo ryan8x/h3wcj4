63207558453f0b7538a948498f615bbbab4ab3c6
ryan liang <ryan@ryans-MacBook.local>
2017-08-23 23:41:28 -0700

    solved reading file and random list item task

---
+++ main/java/com/ryanliang/knockknock/KKJoke.java
@@ -0,0 +1,21 @@
+package com.ryanliang.knockknock;
+
+public class KKJoke {
+
+	private String clue;
+	private String answer;
+	
+	public KKJoke(String clue, String answer){
+		this.clue = clue;
+		this.answer = answer;
+	}
+
+	public String getClue() {
+		return clue;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+}
--- main/java/com/ryanliang/knockknock/ReadLine.java
+++ main/java/com/ryanliang/knockknock/ReadLine.java
@@ -2,17 +2,33 @@ package com.ryanliang.knockknock;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class ReadLine {
 	
 	private static Scanner input;
+	private static ArrayList<KKJoke> kkJokeList = new ArrayList<KKJoke>(100);
+	private static String delimiter = "###";	
 	
 	public static void main(String[] args) {
 		openFile();
 		readRecords();
 		closeFile();
+		displayJokes();
 
+	}
+
+	private static void displayJokes() {
+		
+		Collections.shuffle(kkJokeList);
+		for (KKJoke joke : kkJokeList){
+			System.out.println(joke.getClue());
+			System.out.println(joke.getAnswer());
+		}
+		
 	}
 
 	private static void openFile() {
@@ -20,15 +36,36 @@ public class ReadLine {
 		try {
 			input = new Scanner(Paths.get("clients.txt"));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.println("Error opening file");
+			//e.printStackTrace();
 		}
 		
 	}
 
 	private static void readRecords() {
-		while (input.nex){
-			System.out.printf("%-10d%-12s%-12s%10.2f%n", input.nextInt(), input.next(), input.next(), input.nextDouble());
+		String clue;
+		String answer;
+		String line;
+		String [] lineParts;
+		try {
+			while (input.hasNextLine()){
+				line = input.nextLine();
+				lineParts = line.split(delimiter);	
+				
+				if (lineParts.length == 2){
+					clue = lineParts[0].trim();
+					answer = lineParts[1].trim();
+					if (clue.length() > 0 && answer.length() > 0){
+					    kkJokeList.add(new KKJoke(clue, answer));
+					}
+				}
+			}
+		} catch (NoSuchElementException e) {
+			System.err.println("File improperly formed");
+			//e.printStackTrace();
+		}
+		catch (IllegalStateException e) {
+			System.err.println("Error reading from file");
 		}
 		
 	}
