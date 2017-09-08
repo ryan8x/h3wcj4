e4427c151aa9a60148ac22d6142ea3313f0d9896
Liang <liangr@ca.metsci.com>
2017-09-07 17:36:28 -0700

    enabled thread safe for connection counter

--- main/java/com/ryanliang/knockknock/ConnectionCounter.java
+++ main/java/com/ryanliang/knockknock/ConnectionCounter.java
@@ -1,17 +1,36 @@
 package com.ryanliang.knockknock;
 
+/**
+ * ConnectionCounter keeps track of current client connections. 
+ * @author Ryan L.
+ * @version $Revision$
+ * @since 1.7
+ */
 public class ConnectionCounter {
 	private static int connectionCounter = 0;
 
+	/**
+	 * This method return the value of the counter.
+	 * @return The value of the counter 
+	 */
 	public static int getConnectionCounter() {
+		//This method not need to be synchronized as it is read only.
 		return connectionCounter;
 	}
 
-	public static void increaseConnectionCounter() {
+	/**
+	 * This method increases the counter by 1. 
+	 */
+	public static synchronized void increaseConnectionCounter() {
+		//This method may not need to be synchronized as server will only accept client connection one at a time.
 		connectionCounter++;
 	}
 	
-	public static void decreaseConnectionCounter() {
+	/**
+	 * This method decreases the counter by 1. 
+	 */
+	public static synchronized void decreaseConnectionCounter() {
+		//This method needs to be synchronized as multiple client connections can be disconnected simultaneously.
 		connectionCounter--;
 	}
 }
